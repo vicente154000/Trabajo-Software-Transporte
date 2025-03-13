dc71a747cf7b78ed3ed5eb2bd6d41ca722023a86
vicente154000 <149714332+vicente154000@users.noreply.github.com>
2025-03-13 08:44:05 +0100

    Update Ejercicio.java

--- TransportateG12/src/transportate/modelo/Ejercicio.java
+++ TransportateG12/src/transportate/modelo/Ejercicio.java
@@ -3,35 +3,4 @@ package transportate.modelo;
 
 public class Ejercicio {
     
-    private int id;
-    private String nombre;
-    private String descripcion;
-    private int duracion;
-    private int intensidad;
-    private Ubicacion ubicacion;
-
-    public Ejercicio(int id, String nombre, String descripcion, int duracion, int intensidad, Ubicacion ubicacion) {
-        this.id = id;
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-        this.duracion = duracion;
-        this.intensidad = intensidad;
-        this.ubicacion = ubicacion;
-    }
-
-    //Getter
-    public int getId() { return id; }
-    public String getNombre() { return nombre; }
-    public String getDescripcion() { return descripcion; }
-    public int getDuracion() { return duracion; }
-    public int getIntensidad() { return intensidad; }
-    public Ubicacion getUbicacion() { return ubicacion; }
-    
-    //Setter
-    public void setId(int id) { this.id = id; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
-    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
-    public void setDuracion(int duracion) { this.duracion = duracion; }
-    public void setIntensidad(int intensidad) { this.intensidad = intensidad; }
-    public void setUbicacion(Ubicacion ubicacion) { this.ubicacion = ubicacion; }
 }
