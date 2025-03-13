c91467c88623320d3480f76302d2262311bc7024
vicente154000 <149714332+vicente154000@users.noreply.github.com>
2025-03-13 08:47:19 +0100

    Update Ejercicio.java

--- TransportateG12/src/transportate/modelo/Ejercicio.java
+++ TransportateG12/src/transportate/modelo/Ejercicio.java
@@ -2,5 +2,10 @@
 package transportate.modelo;
 
 public class Ejercicio {
-    
+    private int id;
+    private String nombre;
+    private String descripcion;
+    private int duracion;
+    private int intensidad;
+    private Ubicacion ubicacion;
 }
