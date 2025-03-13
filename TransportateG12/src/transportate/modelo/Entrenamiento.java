55fb1bf83bfb1131e9adb4e7cf3602d145ab5160
vicente154000 <149714332+vicente154000@users.noreply.github.com>
2025-03-13 08:47:05 +0100

    Update Entrenamiento.java

--- TransportateG12/src/transportate/modelo/Entrenamiento.java
+++ TransportateG12/src/transportate/modelo/Entrenamiento.java
@@ -2,5 +2,10 @@
 package transportate.modelo;
 
 public class Entrenamiento {
-    
+    private int id;
+    private int idUsuario;
+    private int duracion;
+    private int caloriasQuemadas;
+    private Date fecha;
+    private List<Ejercicio> ejercicios;
 }
