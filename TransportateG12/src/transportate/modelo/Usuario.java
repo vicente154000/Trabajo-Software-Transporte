3a406c85be3ff0ce0f391ecbf1d89073ed6d1d53
vicente154000 <149714332+vicente154000@users.noreply.github.com>
2025-03-13 08:46:48 +0100

    Update Usuario.java

--- TransportateG12/src/transportate/modelo/Usuario.java
+++ TransportateG12/src/transportate/modelo/Usuario.java
@@ -2,5 +2,12 @@
 package transportate.modelo;
 
 public class Usuario {
-    
+    private String nombre;
+    private String email;
+    private String contraseña;
+    private List<Entrenamiento> historial;
+    private int id;
+    private int altura;
+    private int edad;
+    private int peso;
 }
