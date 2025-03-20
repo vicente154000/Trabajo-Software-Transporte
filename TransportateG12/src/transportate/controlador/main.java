ef3f91931efdc8f4cf81e2bc3eff1718a2182c8e
vicente154000 <149714332+vicente154000@users.noreply.github.com>
2025-03-20 08:49:00 +0100

    Update main.java

--- TransportateG12/src/transportate/controlador/main.java
+++ TransportateG12/src/transportate/controlador/main.java
@@ -4,6 +4,10 @@ package transportate.controlador;
 public class main {
     
     public static void main(String[] args) {
-        // TODO code application logic here
+        Ubicacion aula221 = new Ubicacion("Aula 221", 40.7128, -74.0060);
+        Ubicacion zonaSofas = new Ubicacion("Zona de sofás", 40.7135, -74.0055);
+
+        System.out.println("Distancia entre " + aula221.getNombre() + " y " + zonaSofas.getNombre() + ": " +
+                aula221.calcularDistancia(zonaSofas) + " km");
     }
 }
