6ca7a80f308c25b07178e9379c5aac148d4c6c69
Yushan2 <148941434+Yushan2@users.noreply.github.com>
2025-03-20 10:56:27 +0100

    Modelo

--- TransportateG12android/app/src/main/java/modelo/Ejercicio.java
+++ TransportateG12android/app/src/main/java/modelo/Ejercicio.java
@@ -1,4 +1,56 @@
 package modelo;
 
 public class Ejercicio {
+    private int id;
+    private String nombre;
+    private String descripcion;
+    private int duracion;
+    private int intensidad;
+    private Ubicacion ubicacion;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public int getIntensidad() {
+        return intensidad;
+    }
+
+    public Ubicacion getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    public void setIntensidad(int intensidad) {
+        this.intensidad = intensidad;
+    }
+
+    public void setUbicacion(Ubicacion ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+
 }
--- TransportateG12android/app/src/main/java/modelo/Entrenamiento.java
+++ TransportateG12android/app/src/main/java/modelo/Entrenamiento.java
@@ -1,4 +1,59 @@
 package modelo;
+import java.util.Date;
+import java.util.List;
 
 public class Entrenamiento {
+    private int id;
+    private int idUsuario;
+    private int duracion;
+    private int caloriasQuemadas;
+    private Date fecha;
+    private List<Ejercicio> ejercicios;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public int getCaloriasQuemadas() {
+        return caloriasQuemadas;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public List<Ejercicio> getEjercicios() {
+        return ejercicios;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    public void setCaloriasQuemadas(int caloriasQuemadas) {
+        this.caloriasQuemadas = caloriasQuemadas;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setEjercicios(List<Ejercicio> ejercicios) {
+        this.ejercicios = ejercicios;
+    }
+
+
 }
+
